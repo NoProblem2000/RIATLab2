78ebc964e72ec8adc9e7cf902d6f1df86eb1e644
ILia Artamonov <fernando94@yandex.ru>
2016-11-28 09:29:24 +0400

    11

--- RIATLab2/Program.cs
+++ RIATLab2/Program.cs
@@ -9,7 +9,7 @@ namespace RIATLab2
             ISerializer iSerializer = new JsonSerializer();
             var dom = "127.0.0.1";            
             var port = int.Parse(Console.ReadLine());
-
+            
             Client client = new Client(iSerializer, dom, port);
             client.Ping();
             Input input = client.GetInputData();
